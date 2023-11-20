94cd3a03acbb6feecd2e8330d9fcbed2b476755a
pang0620 <129kjy@gmail.com>
2023-11-20 22:29:45 +0900

    frontend file mv-ed

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<?php
-require_once("select.php");
-?>
-
-<html lang="ko">
+<!DOCTYPE html>
+<html>
     <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>DDT</title>
+        <title>2조의 캡스톤</title>
         <style>
             @import "font_ROCAF.css";
             *{
@@ -28,116 +23,105 @@ require_once("select.php");
                 text-align: center;
             }
 
+            a{
+                text-decoration-line: none;
+                color: white;
+            }
+
+            a:hover{
+                color: rgb(255, 255, 125);
+            }
+
+            #title{
+                padding: 40px 0px 20px 0px;
+            }
+
             #all{
+                width:1500px;
+                height: 1100px;
                 background-color: rgb(221, 239, 255);
-                padding: 20px 50px 50px 50px;
+                font-size: 50px;
             }
 
             .in{
-                width: 150px;
-                margin: 8px 4px;
-                padding: 30px 0px 0px 0px;
+                padding: 50px 0px 0px 0px;
             }
 
-            #left{
-                float: left;
+            #up{
+                margin: 0px 0px 10px 0px;
             }
 
-            #left1{
-                width: 150px;
-                height: 170px;
-                margin: 8px 4px;
-                background-color: rgb(129, 255, 127);
-            }
-
-            #left2{
-                height: 110px;               
-                background-color: rgb(116, 226, 190);
-            }
-
-            #center{
-                float: left;
-            }
-
-            #center1{               
-                height: 70px;
+            #up1{
+                display: inline-block;
+                width: 700px;
+                height: 350px;
                 background-color: rgb(137, 212, 136);
             }
 
-            #center2{                
-                height: 210px;
-                background-color: rgb(212, 252, 212);
+            #up2{
+                display: inline-block;
+                width: 700px;
+                height: 350px;               
+                background-color: rgb(116, 226, 190);
             }
 
-            #right{
-                float: left;
-            }
-
-            #right1{
-                height: 120px;
+            #down1{
+                display: inline-block;
+                width: 450px;
+                height: 450px;
                 background-color: rgb(44, 150, 114);
             }
-            #right2{
-                height: 160px;
+
+            #down2{
+                display: inline-block;
+                width: 400px;
+                height: 450px;
                 background-color: rgb(110, 188, 162);
             }
 
-            #window{
-                margin: 0px 0px 0px 0px ;
-            }
-
-            button{
-                width: 150px;
-                height: 50px;
-                border-radius: 30px;
-                border: 3px solid white;
-                background-color: aqua;
-                color: white;
-                font: 30px Arial;
+            #down3{
+                display: inline-block;
+                width: 535px;
+                height: 450px;
+                background-color: rgb(167, 255, 167);
             }
         </style>
     </head>
     <body>
         <section>
             <div id="all">
-                <div>
-                    <h3>야외 기상 상황</h3>
+                <div id="title">
+                    야외 기상 상황
                 </div>
-                <div id='left'>
-                    <div id="left1" class="in">
-		        온도 <?php echo $temp ?>
-                    </div>
-                    <div id="left2" class="in">
-                        습도 <?php echo $humid ?>
-                    </div>
+                <div id="up">
+                    <a href="미세먼지.html">
+                        <div id="up1" class="in">
+			미세먼지 <?php if((double)$dust>17) {echo "<strong style='color:red;'>$dust</strong>"; } else { echo $dust; }?>
+                        </div>
+                    </a>
+                    <a href="온도.html">
+                        <div id="up2" class="in">
+				온도 <?php echo $temp ?>
+                        </div>
+                    </a>
                 </div>
-                <div id="center">
-                    <div id="center1" class="in">
-		    미세먼지
-<?php 
-	if((double)$dust>17) { 
-		echo "<strong style='color: red;'>$dust</strong>"; 
-	} else {
-		echo $dust;
-}?>
-                    </div>
-                    <div id="center2" class="in">
-                        기상청
-                    </div>
-                </div>
-                <div id="right">
-                    <div id="right1" class="in">
-                        1시간 강수량 <?php echo $prec ?>
-                    </div>
-                    <div id="right2" class="in">
-                        강수확률 <?php echo $prob ?>
-                    </div>
-                </div>
-                <div id="window">
-                    창문 개폐 상태<br><br>
-                    <button>
-                        OPEN
-                    </button>
+                <div id="down">
+                    <a href="1시간강수량.html">
+                        <div id="down1" class="in">
+                            1시간 강수량 <?php echo $prec ?>
+                        </div>
+                    </a>
+                    <a href="강수확률.html">
+                        <div id="down2" class="in">
+                            강수 확률 <?php echo $prob ?>
+                        </div>
+                    </a>
+                    <a href="습도.html">
+                        <div id="down3" class="in">
+                            습도 <?php echo $hum ?>
+                        </div>
+                    </a>
+                    
                 </div>
             </div>
         </section>
