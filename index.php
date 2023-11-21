846486fa88b3b64796b6fb76c595e267f33240b4
pang0620 <129kjy@gmail.com>
2023-11-22 01:18:51 +0900

    frontend v2

--- index.php
+++ index.php
@@ -1,52 +1,59 @@
-	<?php
-	require_once("select.php");
-	?>
+<?php
+require_once("select.php");
+?>
 
-	<!DOCTYPE html>
-	<html lang="ko">
-	    <head>
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
 		<title>TEAM DDT</title>
-		<meta charset="utf-8">
+		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="style.css">
-	    </head>
-	    <body>
+	</head>
+	<body>
 		<section>
-		    <div id="all">
-			<div id="title">
-				TEAM DDT
-			</div>
-			<div id="up">
-			    <a href="미세먼지.html">
-				<div id="up1" class="in">
-				미세먼지 <?php if((double)$dust>17) {echo "<strong style='color:red;'>$dust</strong>"; } else { echo "<br><br>$dust"; }?>
+			<h1 style="font-size: 200%;">TEAM DDT</h1>
+			<div id="all">
+				<div id="title">
 				</div>
-			    </a>
-			    <a href="온도.html">
-				<div id="up2" class="in">
-					온도 <?php echo "<br><br>$temp" ?>
+
+				<div id="up">
+					<a>
+						<div id="up1" class="in">
+							<div id="up1sub" class="sub">😷</div>
+							미세먼지 <?php if((double)$dust>17) {echo "<strong style='color:red;'>$dust</strong>"; } else { echo "<br><br>$dust"; }?>
+						</div>
+					</a>
+					<a>
+						<div id="up2" class="in">
+							<div id="up2sub" class="sub">🌡️</div>
+							온도 <?php echo "<br><br>$temp" ?>
+						</div>
+					</a>
 				</div>
-			    </a>
+
+				<div id="down">
+					<a>
+						<div id="down1" class="in">
+							<div id="down1sub" class="sub">🌧️</div>
+							1시간 강수량 <?php echo "<br><br>$prec" ?>
+						</div>
+					</a>
+				<a>
+
+						<div id="down2" class="in">
+							<div id="down1sub" class="sub">☂️</div>
+							강수 확률 <?php echo "<br><br>$prob" ?>
+						</div>
+					</a>
+					<a>
+						<div id="down3" class="in">
+							<div id="down1sub" class="sub">🌫️</div>
+							습도 <?php echo "<br><br>$humid" ?>
+						</div>
+					</a>
+				</div>
 			</div>
-			<div id="down">
-                    <a href="1시간강수량.html">
-                        <div id="down1" class="in">
-                            1시간 강수량 <?php echo "<br><br>$prec" ?>
-                        </div>
-                    </a>
-                    <a href="강수확률.html">
-                        <div id="down2" class="in">
-                            강수 확률 <?php echo "<br><br>$prob" ?>
-                        </div>
-                    </a>
-                    <a href="습도.html">
-                        <div id="down3" class="in">
-                            습도 <?php echo "<br><br>$humid" ?>
-                        </div>
-                    </a>
-                    
-                </div>
-            </div>
-        </section>
-    </body>
+		</section>
+	</body>
 </html>
